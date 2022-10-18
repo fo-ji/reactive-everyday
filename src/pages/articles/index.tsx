869edabc36325b5b437b1c6eaf3dd8073504f374
fo-ji <fumiya1753@gmail.com>
2022-10-18 18:26:48 +0900

    add tmp index pages

---
+++ src/pages/articles/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next'
+
+const ArticlesList: NextPage = () => {
+  return <h1>ArticlesList</h1>
+}
+
+export default ArticlesList
---
+++ src/pages/tags/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next'
+
+const TagsList: NextPage = () => {
+  return <h1>TagsList</h1>
+}
+
+export default TagsList
