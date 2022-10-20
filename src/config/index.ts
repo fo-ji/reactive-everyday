28dc415213ef408d5f74dcee7f509c438e1aafea
fo-ji <fumiya1753@gmail.com>
2022-10-20 18:03:34 +0900

    add and set notion api

---
+++ src/config/index.ts
@@ -0,0 +1,2 @@
+export const CLIENT_KEY = process.env.NOTION_KEY as string
+export const DATABASE_ID = process.env.NOTION_DATABASE_ID as string
---
+++ src/lib/notion.ts
@@ -0,0 +1,5 @@
+import { Client } from '@notionhq/client'
+
+import { CLIENT_KEY } from '@/config'
+
+export const notion = new Client({ auth: CLIENT_KEY })
