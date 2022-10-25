49c9b6f2b2d289fed2bc7b7b505c7f07a8e8f94e
fo-ji <fumiya1753@gmail.com>
2022-10-25 11:22:27 +0900

    fix notion blocks style

--- src/features/articles/components/Blocks.tsx
+++ src/features/articles/components/Blocks.tsx
@@ -1,9 +1,10 @@
 import NotionBlocks, { BlockType } from 'notion-block-renderer'
+import { monokai } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
 
 interface BlocksProps {
   blocks: BlockType[]
 }
 
 export const Blocks: React.FC<BlocksProps> = ({ blocks }) => {
-  return <NotionBlocks blocks={blocks} isCodeHighlighter />
+  return <NotionBlocks blocks={blocks} isCodeHighlighter syntaxHighlighterCSS={monokai} />
 }
