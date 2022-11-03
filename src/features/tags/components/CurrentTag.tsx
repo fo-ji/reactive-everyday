97a7fe1ea7a5ec207b35ffc4bd04d9f9a0b7fa61
fo-ji <fumiya1753@gmail.com>
2022-11-03 11:21:08 +0900

    update sp layout in tags page

--- src/features/tags/components/CurrentTag.tsx
+++ src/features/tags/components/CurrentTag.tsx
@@ -5,7 +5,7 @@ interface TagProps {
 }
 
 export const CurrentTag: React.FC<TagProps> = ({ tag }) => (
-  <div className='flex w-fit gap-6 rounded-xl bg-white p-6 text-lg font-normal tracking-wide text-placeholder'>
+  <div className='flex w-fit gap-3 rounded-xl bg-white p-3 text-sm font-normal tracking-wide text-placeholder sm:gap-6 sm:p-6 sm:text-lg'>
     <div>selected: </div>
     <Tag>{tag}</Tag>
   </div>
--- src/features/tags/components/Tags.tsx
+++ src/features/tags/components/Tags.tsx
@@ -6,10 +6,10 @@ interface TagsProps {
 }
 
 export const Tags: React.FC<TagsProps> = ({ tags }) => (
-  <div className='flex flex-wrap justify-between gap-5 rounded-xl bg-white p-6'>
+  <div className='flex flex-wrap justify-start gap-4 rounded-xl bg-white p-4 sm:gap-8 sm:p-6'>
     {tags.map((tag, idx) => (
       <Link href={`/tags/${tag}`} key={idx}>
-        <Tag className='text-lg font-normal tracking-wide text-link'>{tag}</Tag>
+        <Tag className='text-sm font-normal tracking-wide text-link sm:text-lg'>{tag}</Tag>
       </Link>
     ))}
   </div>
