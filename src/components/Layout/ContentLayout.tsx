cef3bf3eaadc623585dc0b903831810b39d571b3
fo-ji <fumiya1753@gmail.com>
2022-12-29 10:33:45 +0900

    update contentlayout

--- src/components/Layout/ContentLayout.tsx
+++ src/components/Layout/ContentLayout.tsx
@@ -11,12 +11,14 @@ export const ContentLayout: React.FC<ContentLayoutProps> = ({
   children,
   description = 'ReactやNextなどフロントエンドを中心に日々のキャッチアップを綴っています。バックエンドはNestJSにとても興味があって更新頻度高めになりそうです。サンプルなどで実装する環境は基本的にはDockerで構築しています。'
 }) => (
-  <div className='m-auto max-w-4xl'>
+  <>
     <Head>
       <title>{title}</title>
       <meta name='description' content={description} />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
       <link rel='icon' href='/favicon.png' />
     </Head>
-    {children}
-  </div>
+    <div className='m-auto max-w-4xl'>{children}</div>
+  </>
 )
