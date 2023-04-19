ff7b3dfa87b045f70ad3fca024b1042ecf38056d
fo-ji <fumiya1753@gmail.com>
2023-04-19 19:01:00 +0900

    fix import hero/icons

--- src/components/Notifications/Notification.tsx
+++ src/components/Notifications/Notification.tsx
@@ -4,8 +4,8 @@ import {
   ExclamationCircleIcon,
   InformationCircleIcon,
   XCircleIcon
-} from '@heroicons/react/outline'
-import { XIcon } from '@heroicons/react/solid'
+} from '@heroicons/react/24/outline'
+import { XMarkIcon } from '@heroicons/react/24/solid'
 import { Fragment } from 'react'
 
 import { Button } from '../Elements/Button'
@@ -57,7 +57,7 @@ export const Notification = ({
                 }}
                 className='inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
               >
-                <XIcon className='h-5 w-5' />
+                <XMarkIcon className='h-5 w-5' />
               </Button>
             </div>
           </div>
