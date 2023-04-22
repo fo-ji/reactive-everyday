68939f1b783d06d327b7d9b4616e6fe418de7a72
fo-ji <fumiya1753@gmail.com>
2023-04-22 12:19:55 +0900

    fix layout

--- src/components/Elements/Card/Card.tsx
+++ src/components/Elements/Card/Card.tsx
@@ -11,18 +11,24 @@ interface CardProps {
 }
 
 export const Card: React.FC<CardProps> = ({ title, cover, date, tags }) => (
-  <div className='border-b border-placeholder py-2 px-3 sm:h-40 sm:py-4 sm:px-5'>
+  <div className='border-b border-placeholder px-3 py-2 sm:min-h-[160px] sm:px-5 sm:py-4'>
     <div className='flex flex-col gap-4 sm:flex-row sm:gap-8'>
-      <div className='text-center sm:text-right'>
-        <Image src={cover} width={128} height={128} alt='card' objectFit='contain' />
+      <div className='flex h-32 justify-center'>
+        <Image
+          className='object-contain'
+          src={cover}
+          width={128}
+          height={128}
+          alt='記事で扱うメイン技術のイメージ'
+        />
       </div>
       <div className='flex flex-col justify-between gap-2 tracking-wide sm:w-[calc(100%_-_144px)] sm:gap-4'>
         <div className='flex justify-end gap-4 text-sm font-normal text-placeholder sm:gap-8 sm:text-lg'>
           <div>{date}</div>
           <div>{formatDiffDate(date)}</div>
         </div>
-        <div className='text-center text-base font-semibold text-main sm:text-xl'>{title}</div>
-        <div className='flex justify-end gap-4 sm:gap-8'>
+        <div className='text-base font-semibold text-main sm:text-xl'>{title}</div>
+        <div className='flex flex-wrap justify-end gap-2 sm:gap-8'>
           {tags.map((tag, idx) => (
             <Tag key={idx}>{tag}</Tag>
           ))}
